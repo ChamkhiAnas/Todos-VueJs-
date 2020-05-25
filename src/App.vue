3f1affa8bac5828060e51fc79156d9275eccb232
ChamkhiAnas <mahmoudbennouna@gmail.com>
2020-05-25 19:26:43 +0000

    BasicTodolistVueJs

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Todos></Todos>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todos from './components/Todos.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Todos
   }
 }
 </script>
---
+++ src/components/Todos.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="todos ">
+    <h1>Todos</h1>
+
+    <form @submit="submitForm">  
+
+    <input type="text" v-model="text">
+    <button class="add" type="button">Add</button>
+
+    </form>
+            <ul>
+                <li v-for="(todo,index) in todos" :key="index">
+                        <input class="toggle" type="checkbox" v-model="todo.completed">
+                        <span :class="{completed:todo.completed}">
+                            {{todo.text}}
+                        </span>
+                        <button  @click="deleteTodo(index)" class="delete" type="button">Delete</button>
+                </li>
+            </ul>
+  </div>
+</template>
+
+
+
+<script>
+export default {
+  name: 'Todos',
+  data(){
+      return {
+          text:"",
+          todos:[
+              {
+                   text:'My todo one',
+                   completed: false
+              },
+              {
+                   text:'My todo Two',
+                   completed: false
+              },
+              {
+                   text:'My todo Three',
+                   completed: false
+              },
+          ]
+      }
+  },
+  props: {
+    msg: String
+  },
+  methods:{
+      deleteTodo:function(index){
+          this.todos.splice(index, 1);
+      },
+      submitForm:function(e){
+          this.todos.push({
+              text:this.text,
+              completed:false
+          });
+          e.preventDefault();
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+    ul{
+        list-style:none;
+        padding:0;
+        
+    }
+
+    li{
+        padding:10px 0;
+        border-bottom:1px #ccc solid;
+    }
+
+    .completed{
+        text-decoration:line-through;
+    }
+
+    .delete{
+        background-color: #e74c3c;
+        border: none;
+        font-family: "Barlow Condensed";
+        color: white;
+    }
+    .add{
+        background-color: #27ae60;
+        border: none;
+        font-family: "Barlow Condensed";
+        font-weight:700;
+        color: white;
+        font-size:15px;
+        margin-left:10px
+    }
+ 
+
+</style>
